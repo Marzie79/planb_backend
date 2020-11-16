d777cb89b9a42b5279530c32a5cd9d13fcb88af8
Nastaran <nastaran.akbari02@gmail.com>
2020-11-16 21:37:35 +0330

    changed views.py and urls.py for Profile

--- planB_backend/urls.py
+++ planB_backend/urls.py
@@ -17,15 +17,15 @@ schema_view = get_schema_view(
     public=True,
 )
 
-
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', include('accounts.urls')),
-            #comment versioning
-            #comment versioning
-            #      path('api/v1/', include((v1, 'v1'), namespace='v1')),
+                  # comment versioning
+                  # comment versioning
+                  #      path('api/v1/', include((v1, 'v1'), namespace='v1')),
                   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
                   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
                   path('swagger.json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-                  path('update-profile/<int:pk>/', UpdateUser.as_view(), name='auth_update_profile')
+                  path('profile/', ProfileUser.as_view({'get': 'retrieve', 'put': 'retrieve'}),
+                       name='auth_profile'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
