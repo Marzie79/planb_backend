4192996ad39c9851dce2a63238bd26ce55127869
bardiadorry <bardiadorry@gmail.com>
2020-12-15 10:58:11 +0330

    some debug

--- core/exceptionHandler.py
+++ core/exceptionHandler.py
@@ -3,6 +3,6 @@ from rest_framework.views import exception_handler
 
 def api_exception_handler(exc, context):
     response = exception_handler(exc, context)
-    if response is not None and 'code' not in response.data:
+    if response is not None and 'code' not in response.data and 'detail' in response.data:
          response.data['code'] = exc.get_codes()
     return response
