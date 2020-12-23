1b312704b517fb72a8006b6164c10b9dc69da522
Nastaran <nastaran.akbari02@gmail.com>
2020-12-23 20:52:00 +0330

    add filter to user-project

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 router = DefaultRouter()
 router.register(r'user-project', UserProjectView)
+# router.register(r'user-project/filter', StatusUserProjectView)
 urlpatterns = router.urls
