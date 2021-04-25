fcedde26f488cb1552f5152cdb72778379bf7f7d
Nastaran <nastaran.akbari02@gmail.com>
2021-04-25 23:32:33 +0430

    remove authentication tests

--- accounts/tests/test_views/test_view_profile.py
+++ accounts/tests/test_views/test_view_profile.py
@@ -6,29 +6,12 @@ from accounts.models import User
 
 class ProfileViewTest(APITestCase):
     def setUp(self):
-        self.user = baker.make(User, resume="resume.pdf")
         self.client = APIClient()
-        self.data = str(self.user.resume)
-        self.client.force_authenticate(user=self.user)
         self.url = '/user/profile/resume/'
-
-    def test_destroy_resume_authenticated(self):
-        response = self.client.delete(self.url, self.data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-
-    def test_destroy_resume_un_authenticated(self):
-        self.client.force_authenticate(user=None)
-        response = self.client.delete(self.url, self.data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_destroy_resume_not_exist(self):
         user = baker.make(User, resume=None)
-        data = str(user.resume)
-        response = self.client.delete(self.url, data, format='json')
+        self.client.force_authenticate(user=user)
+        response = self.client.delete(self.url, format='json')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_destroy_resume_by_another_user(self):
-        random_user = User.objects.create_user(username="Nastaran", password="1234", email="nastaran@gmail.com")
-        self.client.force_authenticate(user=random_user)
-        response = self.client.delete(self.url, self.data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
