7651302731f085fffb48dc4914092c8daf5480e8
Nastaran <nastaran.akbari02@gmail.com>
2021-06-05 02:46:30 +0430

    test_view_profile

--- dashboard/tests/test_views/test_view_profile.py
+++ dashboard/tests/test_views/test_view_profile.py
@@ -10,34 +10,19 @@ class ProfileViewTest(BaseAPITestCase):
     def setUp(self):
         self.client = APIClient()
 
-    def test_destroy_resume_does_exist(self):
+    def test_destroy_resume(self):
         self.url = reverse('profile_resume')
         self.user = baker.make(User, resume='resume.pdf')
         self.client.force_authenticate(user=self.user)
         response = self.client.delete(self.url, format='json')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
-    def test_destroy_resume_does_not_exist(self):
-        self.url = reverse('profile_resume')
-        self.user = baker.make(User, resume=None)
-        self.client.force_authenticate(user=self.user)
-        response = self.client.delete(self.url, format='json')
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-
-    def test_destroy_picture_does_exist(self):
+    def test_destroy_picture(self):
         self.url = reverse('profile_picture')
         self.user = baker.make(User, avatar='picture.png')
         self.client.force_authenticate(user=self.user)
         response = self.client.delete(self.url, format='json')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-
-    def test_destroy_picture_does_not_exist(self):
-        self.url = reverse('profile_picture')
-        self.user = baker.make(
-            User)  # User's avatar is "User.IMAGE_PROCESS.get('default')" when the user has not specified any picture.
-        self.client.force_authenticate(user=self.user)
-        response = self.client.delete(self.url, format='json')
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_profile_endpoints(self):
         self.assertResponse(self.client.get(reverse('profile_brief')))
