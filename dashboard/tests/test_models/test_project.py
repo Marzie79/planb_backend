929cf156543e4a8527f82a2b9e228299ced2f0ca
NimG_Unit <ranjinima270@gmail.com>
2021-06-08 18:01:07 +0430

    Added user_project test cases!

--- dashboard/tests/test_models/test_project.py
+++ dashboard/tests/test_models/test_project.py
@@ -90,8 +90,32 @@ class TestTeamProject(TestCase):
         self.user = baker.make(User)
         self.user_project = baker.make(UserProject, user=self.user, project=self.project)
 
-    def test_has_object_read_permission(self):
-        self.user_project.status = 'CREATOR'
-        self.assertTrue(self.user_project.object_read_permission({'status' : 'PENDING'},self.user))
+    def test_object_read_permission(self):
+        other_user = baker.make(User)
+        self.assertEqual(self.user_project.object_read_permission({'status' : 'PENDING'},self.user), False)
+        self.assertEqual(self.user_project.object_read_permission({'status' : 'PENDING'},other_user), False)
+        self.assertEqual(self.user_project.object_read_permission({'status' : 'DECLINED'},self.user), True)
 
+    def test_object_update_permission(self):
+        project = baker.make(Project, status = "ENDED")
+        other_user = baker.make(User)
+        user_project = baker.make(UserProject, user=self.user, project=project)
+        self.assertEqual(user_project.object_update_permission(self.user, other_user, 'ACCEPTED'), False)
+        
+        new_project = baker.make(Project, status = 'STARTED')
+        new_user_project = baker.make(UserProject, user=self.user, project=new_project, status='ADMIN')
+        self.assertEqual(new_user_project.object_update_permission(self.user, self.user, 'DELETED'), True)
+        self.assertEqual(new_user_project.object_update_permission(self.user, other_user, 'DECLINED'), True)
+        self.assertEqual(new_user_project.object_update_permission(other_user, self.user, 'ACCEPTED'), False)
 
+    def test_object_create_permission(self):
+        project = baker.make(Project, status = "ENDED")
+        other_user = baker.make(User)
+        user_project = baker.make(UserProject, user=self.user, project=project)
+        self.assertEqual(user_project.object_create_permission(self.user, other_user, 'PENDING'), False)
+
+        new_project = baker.make(Project, status = 'STARTED')
+        new_user_project = baker.make(UserProject, user=self.user, project=new_project, status='ADMIN')
+        self.assertEqual(new_user_project.object_create_permission(self.user, self.user, 'PENDING'), True)
+        self.assertEqual(new_user_project.object_create_permission(self.user, self.user, 'ACCEPTED'), True)
+        self.assertEqual(new_user_project.object_create_permission(other_user, self.user, 'ACCEPTED'), False)
