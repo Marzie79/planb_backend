e7257d195760da83e8afe5e97a26e04affdc22ce
bardiadorry <bardiadorry@gmail.com>
2021-07-18 22:16:10 +0430

    add locust test

--- locustfiles.py
+++ locustfiles.py
@@ -12,11 +12,14 @@ django.setup()
 # import json
 
 SIGNIN_URL = reverse('token_obtain_pair')
-VERIFY_URL = reverse('verify')
-SIGNUP_URL = reverse('signup')
+PROJECT_DETAIL_URL = reverse('project-detail', kwargs={'slug': 'تیکتینگ'})
+PROJECT_MEMBER_URL = reverse('project_members-list', kwargs={'slug_slug': 'تیکتینگ'})
+USER_DETAIL_URL = reverse('user-detail', kwargs={'username': 'superuser'})
+USER_PROJECT_URL = reverse('user_info_projects-list', kwargs={'slug_username': 'superuser'})
+
 
 def prepare_login_data():
-    username = "nimapr"
+    username = "superuser"
     password = "123"
     data = {"username": username, "password": password}
     return data
@@ -27,11 +30,30 @@ class UserSet(HttpUser):
     def login(self):
         self.client.post(SIGNIN_URL, data=prepare_login_data())
 
-    # @task
-    # def signup(self):
-    #     self.client.post(SIGNUP_URL, data={"email": "paryfardnim@gmail.com"})
 
-    # @task
-    # def varify(self):
-    #     self.client.post(VERIFY_URL, data=)
+class ProjectDetailSet(HttpUser):
+    @task
+    def project_detail(self):
+        self.client.get(PROJECT_DETAIL_URL)
 
+    @task
+    def project_member_detail(self):
+        self.client.get(PROJECT_MEMBER_URL)
+
+
+class UserDeailSet(HttpUser):
+    @task
+    def user_detail(self):
+        self.client.get(USER_DETAIL_URL)
+
+    @task
+    def user_project_detail(self):
+        self.client.get(USER_PROJECT_URL)
+
+# @task
+# def signup(self):
+#     self.client.post(SIGNUP_URL, data={"email": "paryfardnim@gmail.com"})
+
+# @task
+# def varify(self):
+#     self.client.post(VERIFY_URL, data=)
